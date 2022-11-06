584201e323b267ee0709c17d2d98a99893e28f0d
stubakka78 <alderab08@hotmail.com>
2022-11-06 13:40:47 -0600

    Reference Materials

---
+++ storage/lanaenvironment.py
@@ -0,0 +1,87 @@
+import allure
+from allure_commons.types import AttachmentType
+from app.application import Application
+
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.events import EventFiringWebDriver
+
+from support.logger import logger, MyListener
+
+# Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+bs_user = ''
+bs_key = ''
+
+# Allure command:
+# behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/product_page.feature
+
+
+def browser_init(context, test_name):
+    """
+    :param context: Behave context
+    :param test_name: scenario.name
+    """
+    context.driver = webdriver.Chrome(executable_path='')
+    # context.driver = webdriver.Firefox(executable_path='/Users/svetlanalevinsohn/JobEasy/12-python-selenium-automation/geckodriver')
+    # context.driver = webdriver.Firefox(executable_path='/Users/svetlanalevinsohn/JobEasy/11-python-selenium-automation/geckodriver')
+    # context.driver = webdriver.Safari()
+
+    ## HEADLESS MODE ####
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # context.driver = webdriver.Chrome(
+    #     chrome_options=options,
+    #     executable_path='/Users/svetlanalevinsohn/JobEasy/12-python-selenium-automation/chromedriver'
+    # )
+
+    ### EventFiringWebDriver - log file ###
+    ### for drivers ###
+    # context.driver = EventFiringWebDriver(
+    #     webdriver.Chrome(
+    #         executable_path='/Users/svetlanalevinsohn/JobEasy/12-python-selenium-automation/chromedriver'
+    #     ),
+    #     MyListener())
+    # for headless mode ###
+    # context.driver = EventFiringWebDriver(webdriver.Chrome(chrome_options = options), MyListener())
+
+    # for browerstack ###
+    # desired_cap = {
+    #     'browser': 'Firefox',
+    #     'os_version': '11',
+    #     'os': 'Windows',
+    #     'name': test_name
+    # }
+    # bs_user = ''
+    # bs_key = ''
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    # context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
+
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(5)
+    context.driver.wait = WebDriverWait(context.driver, 10)
+
+    context.app = Application(context.driver)
+
+
+def before_scenario(context, scenario):
+    # print('\nStarted scenario: ', scenario.name)
+    logger.info(f'Started scenario: {scenario.name}')
+    browser_init(context, scenario.name)
+
+
+def before_step(context, step):
+    # print('\nStarted step: ', step)
+    logger.info(f'Started step: {step}')
+
+
+def after_step(context, step):
+    if step.status == 'failed':
+        logger.error(f'Step failed: {step}')
+        print('\nStep failed: ', step)
+        # Mark test case as FAILED on BrowserStack: context.driver.execute_script( 'browserstack_executor: {"action":
+        # "setSessionStatus", "arguments": {"status":"failed", "reason": "Step failed"}}')
+
+
+def after_scenario(context, feature):
+    context.driver.delete_all_cookies()
+    context.driver.quit()
